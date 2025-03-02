84fe4318e9b365a858262ac83b7b730655bbd662
Henrique Costa <henrique.costa@flowih.com>
2025-03-02 16:22:15 -0300

    Refactor distributor page layout and implement distributor creation handling

--- src/app/(distributors)/page.tsx
+++ src/app/(distributors)/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-// import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from "/src/@/components/ui/table";
 import { AppSidebar } from "/src/@/components/app-sidebar";
 import {
   Breadcrumb,
@@ -27,7 +26,6 @@ import { DataTable } from "../../components/data-table";
 interface Distributor {
   DISTRIBUTOR_ID: string;
   AVATAR: string | null;
-
   PLAN_TYPE: "pro" | "master" | "starter";
   ADDRESS: string;
   LATITUDE: number;
@@ -60,6 +58,11 @@ export default function Page() {
     }
   }
 
+  async function handleCreate(id: string) {
+    const response = await api.get<Distributor>(`/api/distributors/${id}`);
+    setDistributors((prevDistributors) => [...prevDistributors, response.data]);
+  }
+
   useEffect(() => {
     if (!isLoading && !isAuthenticated) {
       router.push("/login");
@@ -68,14 +71,11 @@ export default function Page() {
 
   useEffect(() => {
     if (isAuthenticated && !isLoading) {
-      const fetchDistributors = () => {
-        api.get("/api/distributors")
-          .then((response) => {
-            setDistributors(response.data);
-          })
-          .catch(console.error);
-      };
-      fetchDistributors();
+      api.get("/api/distributors")
+        .then((response) => {
+          setDistributors(response.data);
+        })
+        .catch(console.error);
     }
   }, [isAuthenticated, isLoading]);
 
@@ -91,72 +91,37 @@ export default function Page() {
     return null;
   }
 
-
   return (
-    isLoading ? <div className="flex justify-center items-center"><Loading /></div> :
-      <SidebarProvider>
-        <AppSidebar />
-        <SidebarInset>
-          <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
-            <div className="flex items-center gap-2 px-4">
-              <SidebarTrigger className="-ml-1" />
-              <Separator orientation="vertical" className="mr-2 h-4" />
-              <Breadcrumb>
-                <BreadcrumbList>
-                  <BreadcrumbItem className="hidden md:block">
-                    <BreadcrumbLink href="#">AP Pro</BreadcrumbLink>
-                  </BreadcrumbItem>
-                  <BreadcrumbSeparator className="hidden md:block" />
-                  <BreadcrumbItem>
-                    <BreadcrumbPage>Distribuidores</BreadcrumbPage>
-                  </BreadcrumbItem>
-                </BreadcrumbList>
-              </Breadcrumb>
-            </div>
-          </header>
-          <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-            <div className="flex-row flex items-center justify-end">
-              <DialogCreateDistributor onCreate={async (id: string) => {
-                const response = await api.get<Distributor>(`/api/distributors/${id}`);
-                setDistributors((prevDistributors) => [...prevDistributors, response.data]);
-              }} />
-            </div>
-            <div className="min-h-[100vh] flex-1 rounded-xl md:min-h-min lg:px-4 py-2">
-              <DataTable data={distributors} onDelete={(id: string) => handleDelete(id)} />
-              {/* <Table>
-                <TableCaption>A lista de todos os seus distribuidores</TableCaption>
-                <TableHeader>
-                  <TableRow>
-                    <TableHead>Primeiro Nome</TableHead>
-                    <TableHead>Segundo Nome</TableHead>
-                    <TableHead>Tipo de Plano</TableHead>
-                    <TableHead>Email</TableHead>
-                    <TableHead>Telefone</TableHead>
-                    <TableHead>Whatsapp</TableHead>
-                    <TableHead>Endereço</TableHead>
-                    <TableHead>CEP</TableHead>
-                    <TableHead>Região</TableHead>
-                  </TableRow>
-                </TableHeader>
-                <TableBody>
-                  {distributors.map((distributor: Distributor) => (
-                    <TableRow key={distributor.DISTRIBUTOR_ID}>
-                      <TableCell>{distributor.FIRST_NAME}</TableCell>
-                      <TableCell>{distributor.LAST_NAME}</TableCell>
-                      <TableCell>{distributor.PLAN_TYPE}</TableCell>
-                      <TableCell>{distributor.EMAIL}</TableCell>
-                      <TableCell>{distributor.PHONE_NUMBER}</TableCell>
-                      <TableCell>{distributor.WHATSAPP_NUMBER}</TableCell>
-                      <TableCell>{distributor.ADDRESS}</TableCell>
-                      <TableCell>{distributor.POSTAL_CODE}</TableCell>
-                      <TableCell>{distributor.REGION}</TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table> */}
-            </div>
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset className="px-5 overflow-x-hidden">
+        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+          <div className="flex items-center gap-2 px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator orientation="vertical" className="mr-2 h-4" />
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem className="hidden md:block">
+                  <BreadcrumbLink href="#">AP Pro</BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator className="hidden md:block" />
+                <BreadcrumbItem>
+                  <BreadcrumbPage>Distribuidores</BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
           </div>
-        </SidebarInset>
-      </SidebarProvider>
+        </header>
+        <div className="flex flex-1 flex-col gap-4 py-4 pt-0">
+          <div className="flex items-center justify-between px-4">
+            <h1 className="text-lg font-semibold">Distribuidores</h1>
+            <DialogCreateDistributor onCreate={handleCreate} />
+          </div>
+          <div className="min-h-[calc(100vh-150px)] flex-1 rounded-xl md:min-h-min lg:px-4 py-2">
+            <DataTable data={distributors} onDelete={handleDelete} />
+          </div>
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
   );
 }
